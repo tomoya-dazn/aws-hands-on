6ce6d37569300ccb35f5c79c5b2b8fd277a9390f
t.sasaki <t.sasaki@future-shop.co.jp>
2022-08-09 16:48:49 +0900

    Update

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,22 +9,130 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+variable "region" {
+}
+
 variable "access_key" {
 }
 
 variable "secret_key" {
 }
 
+variable "default_vpc_id" {
+}
+
+variable "wordpress_ami" {
+}
+
+variable "key_name" {
+}
+
 provider "aws" {
-  region     = "ap-northeast-1"
+  region     = var.region
   access_key = var.access_key
   secret_key = var.secret_key
 }
 
 resource "aws_internet_gateway" "gw" {
-  vpc_id = "vpc-0ba872d46dc4b2213"
+  vpc_id = var.default_vpc_id
   tags = {
     name = "wordpress_internet_gateway"
   }
 }
 
+resource "aws_subnet" "public_1a" {
+  vpc_id                  = var.default_vpc_id
+  cidr_block              = "172.31.0.0/24"
+  availability_zone       = "${var.region}a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-1a"
+  }
+}
+
+resource "aws_subnet" "public_1c" {
+  vpc_id                  = var.default_vpc_id
+  cidr_block              = "172.31.1.0/24"
+  availability_zone       = "${var.region}c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-1c"
+  }
+}
+
+resource "aws_subnet" "private_1a" {
+  vpc_id                  = var.default_vpc_id
+  cidr_block              = "172.31.2.0/24"
+  availability_zone       = "${var.region}a"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "private-1a"
+  }
+}
+
+resource "aws_subnet" "private_1c" {
+  vpc_id                  = var.default_vpc_id
+  cidr_block              = "172.31.3.0/24"
+  availability_zone       = "${var.region}c"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private-1c"
+  }
+}
+
+resource "aws_security_group" "webserver_sg" {
+  name        = "webserver_security_group"
+  description = "Security Group for WordPress server"
+  vpc_id      = var.default_vpc_id
+
+  ingress = [
+    {
+      description      = "HTTP"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    },
+    {
+      description      = "SSH"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress = [{
+    description      = "ALL"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    security_groups  = []
+    self             = false
+  }]
+}
+
+resource "aws_instance" "wordpress_1" {
+  ami                    = var.wordpress_ami
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  subnet_id              = aws_subnet.public_1a.id
+  vpc_security_group_ids = [aws_security_group.webserver_sg.id]
+
+  tags = {
+    Name = "wordpress-1a"
+  }
+}
